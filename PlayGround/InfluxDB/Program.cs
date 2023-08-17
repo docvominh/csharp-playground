c10922873110ada68a7d0c5ae96dea29b43b0e8b
Minh Pham <phamducminh1990@gmail.com>
2023-08-17 11:02:05 +0700

    add InfluxDB quick example

---
+++ PlayGround/InfluxDB/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Writes;
+
+namespace InfluxDB;
+
+public class Program
+{
+    private static readonly Random _random = new Random();
+
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var client = GetClient();
+
+        // Write
+        var writeApi = client.GetWriteApi();
+        for (var i = 100; i < 10000; i++)
+        {
+            var point = PointData.Measurement("myrandom")
+                .Tag("plane", "test-plane")
+                .Field("value", _random.Next(1000, 5000))
+                .Timestamp(DateTime.UtcNow, WritePrecision.Ns);
+
+            writeApi.WritePoint(point, "test-bucket", "organization");
+        }
+
+        writeApi.Flush();
+
+        // Read
+        var query = client.GetQueryApi();
+        var flux = "from(bucket:\"test-bucket\") |> range(start: 0)";
+        var tables = await query.QueryAsync(flux, "organization");
+        var result = tables.SelectMany(table =>
+            table.Records.Select(record =>
+                new AltitudeModel
+                {
+                    Time = record.GetTime().ToString(),
+                    Altitude = int.Parse(record.GetValueByKey("_value").ToString() ?? string.Empty)
+                }));
+
+        foreach (var model in result)
+        {
+            Console.WriteLine(model.DisplayText);
+        }
+    }
+
+    private static InfluxDBClient GetClient()
+    {
+        const string token = "JMNlova9CWwbVfQYtdVBZMAbhKjaCneV3W8QkXfqFLyGHFVxgQLI6KsK6QLdF2xIuzCT0PFy0ZX-42dsKju1cA==";
+        const string bucket = "bucket";
+        const string org = "organization";
+        return new InfluxDBClient("http://localhost:8086", token);
+    }
+}
+
+public class AltitudeModel
+{
+    public string? Time { get; init; }
+    public int Altitude { get; init; }
+    public string DisplayText => $"Plane was at altitude {Altitude} ft. at {Time}.";
+}
---
+++ PlayGround/InfluxDB/readme.md
@@ -0,0 +1,21 @@
+### Getting Started with C# and InfluxDB
+
+https://www.influxdata.com/blog/getting-started-with-c-and-influxdb/
+
+```bash 
+docker pull influxdb:2.0.7
+
+docker run --name influxdb -p 8086:8086 influxdb:2.0.7
+
+
+```
+
+Next, navigate to http://localhost:8086/ on your local machine. Click Get Started and fill in the following values:
+
+User: admin
+
+Password: adminadmin
+
+Organization: organization
+
+Bucket: bucket
